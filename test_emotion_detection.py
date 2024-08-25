ee344ed3ed1121acce5055f8a2784b1cf0088f1b
feramig <feram_caa@hotmail.com>
2024-08-25 00:30:45 -0400

    complete files

---
+++ test_emotion_detection.py
@@ -0,0 +1,21 @@
+from EmotionDetection.emotion_detection import emotion_detector
+import unittest
+
+class TestEmotionDetection(unittest.TestCase):
+    def test_emotion_detection(self):
+        joy = emotion_detector("I am glad this happened")
+        self.assertEqual(joy['dominant_emotion'],'joy')
+
+        anger = emotion_detector("I am really mad about this")
+        self.assertEqual(anger['dominant_emotion'],'anger')
+
+        disgust = emotion_detector("I feel disgusted just hearing about this")
+        self.assertEqual(disgust['dominant_emotion'],'disgust')
+        
+        sadness = emotion_detector("I am so sad about this")
+        self.assertEqual(sadness['dominant_emotion'],'sadness')
+        
+        fear = emotion_detector("I am really afraid that this will happen")
+        self.assertEqual(fear['dominant_emotion'],'fear')
+
+unittest.main()
